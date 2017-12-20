c48df3b2a3550770ccd1b64ed0e5b6a5fe16a3bb
Maru <zhangbinhui@souche.com>
2017-12-20 15:26:54 +0800

    feat: support email notification

--- Sources/config.py
+++ Sources/config.py
@@ -4,7 +4,7 @@
 config = {
     "master": {
         "name": "张斌辉",
-        "email": "zhangbinhui@souche.com",
+        "email": "475435200@qq.com",
         "token": "a992317e7290c8d15b132013776bc03f",
         "black": "每日鲜水果屋"
     }
